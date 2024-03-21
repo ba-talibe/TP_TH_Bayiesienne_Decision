49abe5ba79ac011347096be55f1e84a53478faf0
omarba2015@gmail.com <omarba2015@gmail.com>
2024-03-21 16:10:35 +0100

    started new tranning approa

--- TP5/automatic-trainnig.py
+++ TP5/automatic-trainnig.py
@@ -9,16 +9,17 @@ from fonction_decision import get_predictor
 
 
 if __name__ == '__main__':
-    show(train_data2)
+    show(train_data)
     plt.show()
     # x_min = input("entre x1 et x2 min:").split(' ')
     # x_min = [float(x) for x in x_min]
     # x_max = input("entre x1 et x2 max:").split(' ')
     # x_max = [float(x) for x in x_max]
     x_min, x_max = train_perceptron(train_data, iters=100)
+    
     print(x_min, x_max)
     prediction = get_predictor(x_min, x_max)
-    show(validation_data2)
+    show(validation_data)
     plot_decision(x_min[0],x_max[0],x_min[1],x_max[1], prediction)
-    plt.show()
-    validate(validation_data2, x_min, x_max)
+    plt.show()  
+    validate(validation_data, x_min, x_max)
--- TP5/train_tools.py
+++ TP5/train_tools.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
 
-def train_perceptron(train_data, lr=10**-2, iters = 10**4):
+def train_perceptron(train_data, lr=10**-1, iters = 10**4):
     X = train_data[["x1", "x2"]].to_numpy()
     X = np.hstack([X, np.ones((X.shape[0], 1))])
     y = train_data.y.to_numpy()
 
     theta = np.array([0.0, 0.0, 0.0])
-    activation = lambda x: np.where(x >= 0, 1, 0)
+    activation = lambda x: np.where(x <= 0, 0, 1)
 
     for _ in range(iters):
         for idx, xi in enumerate(X):
@@ -22,8 +22,14 @@ def train_perceptron(train_data, lr=10**-2, iters = 10**4):
     x2_max = -(theta[0]*x1_max + theta[2])/theta[1]
     return [x1_min, x2_min], [x1_max, x2_max]
             
+def moindre_carrés(train_data, lr=10**-1, iters = 10**4):
+    X = train_data[["x1", "x2"]].to_numpy()
+    X = np.hstack([X, np.ones((X.shape[0], 1))])
+    y = train_data.y.to_numpy()
 
-
+    theta = np.array([0.0, 0.0, 0.0])
+    
+            
 
 df = pd.DataFrame()
 
