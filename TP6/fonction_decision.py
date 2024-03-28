c17fc05c0d476156b5c41857509c752c4192cf53
omarba2015@gmail.com <omarba2015@gmail.com>
2024-03-28 15:33:37 +0100

    added tp7 notebbok

--- TP6/fonction_decision.py
+++ TP6/fonction_decision.py
@@ -4,16 +4,16 @@ def get_vector_predictor(mu0, sigma0, mu1, sigma1):
     def predictor(x):
         d0 = np.sum((x-mu0)*(x-mu0), axis=1)
         d1 = np.sum((x-mu1)*(x-mu1), axis=1)
-        pred = d0 <= d1
-        return pred * 1
+        pred = d0 >= d1
+        return pred.astype(int)
     return predictor
 
 def get_predictor(mu0, sigma0, mu1, sigma1):
     def predictor(x):
-        d0 = (x-mu0)*(x-mu0)
+        d0 = (x-mu0).T@(x-mu0)
         d1 = (x-mu1).T@(x-mu1)
-        pred = d0 <= d1
-        return pred * 1
+        pred = d0 >= d1
+        return pred.astype(int)
     return predictor
 
 
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     x = np.array([4, 9])
     mu0 = np.array([1, 1])
     mu1 = np.array([.5, .5])
-    predictor = get_vector_predictor(mu0, 9, mu1, 10)
+    predictor = get_predictor(mu0, 9, mu1, 10)
     print(predictor(x))
     # print((x-mu0))
     # print((x-mu0)*(x-mu0))
